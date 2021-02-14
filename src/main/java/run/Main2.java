b02bb1d7edee7a27cf1a4476f1f80cec1b211fbb
Aleksey Ponomarenko <firebird89@list.ru>
2021-02-14 19:37:54 +0300

    SB HASH MAP in memory удачно перенес

--- src/main/java/run/Main2.java
+++ src/main/java/run/Main2.java
@@ -1,8 +1,23 @@
 package run;
 
+import primaryTasks.Service;
+import primaryTasks.ServiceImpl;
+
 public class Main2 {
     public static void main(String[] args) {
+
         System.out.println("main2");
+
+
+        Service service = ServiceImpl.createNewProxy();
+        for (int i=0; i < 10; i++) {
+            service.doHardWork("key" + i, "value" + 0);
+        }
+
+        for (int i=0; i < 10; i++) {
+            service.doHardWork("key" + i, "value" + 0);
+        }
+
     }
 
 }
